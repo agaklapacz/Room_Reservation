9528bfdb5bbfd7709865997d1b4cd3dc5c2ba7f6
agaklapacz <agnieszka.klapacz.98@gmail.com>
2020-09-01 22:06:29 +0200

    Another commit

--- src/main/java/com/github/agaklapacz/controller/ReservationController.java
+++ src/main/java/com/github/agaklapacz/controller/ReservationController.java
@@ -2,9 +2,11 @@ package com.github.agaklapacz.controller;
 
 import com.github.agaklapacz.logic.ReservationService;
 import com.github.agaklapacz.logic.RoomReservation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -13,9 +15,12 @@ import java.util.List;
 @RequestMapping(value = "/reservations")
 public class ReservationController {
 
+    @Autowired
     private ReservationService reservationService;
 
+    @RequestMapping(method = RequestMethod.GET)
     public String getReservations(@RequestParam(value = "data", required = false)String dateString, Model model) {
+
         return "reservations";
     }
 }
--- src/main/java/com/github/agaklapacz/logic/ReservationService.java
+++ src/main/java/com/github/agaklapacz/logic/ReservationService.java
@@ -3,27 +3,62 @@ package com.github.agaklapacz.logic;
 import com.github.agaklapacz.adapter.GuestRepository;
 import com.github.agaklapacz.adapter.ReservationRepository;
 import com.github.agaklapacz.adapter.RoomRepository;
+import com.github.agaklapacz.model.Guest;
+import com.github.agaklapacz.model.Reservation;
+import com.github.agaklapacz.model.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 @Service
 public class ReservationService {
-    private RoomRepository repository;
+    private RoomRepository roomRepository;
     private GuestRepository guestRepository;
     private ReservationRepository reservationRepository;
 
     public static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
     @Autowired
-    public ReservationService(RoomRepository repository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
-        this.repository = repository;
+    public ReservationService(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
+        this.roomRepository = roomRepository;
         this.guestRepository = guestRepository;
         this.reservationRepository = reservationRepository;
+    }
+
+    public List<RoomReservation> getRoomReservationsForDate(String dateString) {
+        Date date = this.createDateFromDateString(dateString);
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
+        rooms.forEach(room -> {
+            RoomReservation roomReservation = new RoomReservation();
+            roomReservation.setRoomId(room.getId());
+            roomReservation.setRoomName(room.getName());
+            roomReservation.setRoomNumber(room.getNumber());
+            roomReservationMap.put(room.getId(), roomReservation);
+        });
+        Iterable<Reservation> reservations = this.reservationRepository.findByDate(new java.sql.Date(date.getTime()));
+        if (null != reservations) {
+            reservations.forEach(reservation -> {
+                Optional<Guest> guestResponse = this.guestRepository.findById(reservation.getGuestId());
+                if(guestResponse.isPresent()){
+                    Guest guest = guestResponse.get();
+                    RoomReservation roomReservation = roomReservationMap.get(reservation.getId());
+                    roomReservation.setDate(date);
+                    roomReservation.setFirstName(guest.getFirstName());
+                    roomReservation.setLastName(guest.getLastName());
+                    roomReservation.setGuestId(guest.getId());
+                }
+            });
+        }
+        List<RoomReservation> roomReservations = new ArrayList<>();
+        for(Long roomId:roomReservationMap.keySet()) {
+            roomReservations.add(roomReservationMap.get(roomId));
+        }
+        return roomReservations;
     }
 
     private Date createDateFromDateString(String dateString){
