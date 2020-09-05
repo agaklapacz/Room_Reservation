fbdf521d54008e4e00da4a483b0ab43ba8cfb20b
agaklapacz <agnieszka.klapacz.98@gmail.com>
2020-09-05 13:20:10 +0200

    commit

--- src/main/java/com/github/agaklapacz/controller/ReservationServiceController.java
+++ src/main/java/com/github/agaklapacz/controller/ReservationServiceController.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-/*
+
 @RestController
-@RequestMapping(value="/api")*/
+@RequestMapping(value="/api")
 public class ReservationServiceController {
 
-/*
+
     @Autowired
     private ReservationService reservationService;
 
     @RequestMapping(method= RequestMethod.GET, value="/reservations/{date}")
     public List<RoomReservation> getAllReservationsForDate(@PathVariable(value="date")String dateString){
         return this.reservationService.getRoomReservationsForDate(dateString);
-    }*/
+    }
 }
 
